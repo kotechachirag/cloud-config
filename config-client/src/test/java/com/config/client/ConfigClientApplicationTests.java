8cd017c6f387b21d65e0cbf4fef6719f17c54891
kchriag <kchirag@accuratebackground.com>
2017-02-24 02:03:26 -0800

    First Commit

---
+++ config-client/src/test/java/com/config/client/ConfigClientApplicationTests.java
@@ -0,0 +1,18 @@
+package com.config.client;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@WebAppConfiguration
+public class ConfigClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
